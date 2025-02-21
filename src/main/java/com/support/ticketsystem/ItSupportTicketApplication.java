86430ef30de30ad11ee322087d30b252fe2e1a44
Nizar Sbai <nizar1.sbai@gmail.com>
2025-02-21 13:45:54 +0100

    Initial commit

---
+++ src/main/java/com/support/ticketsystem/ItSupportTicketApplication.java
@@ -0,0 +1,13 @@
+package com.support.ticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSupportTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItSupportTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/support/ticketsystem/ItSupportTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.support.ticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSupportTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
