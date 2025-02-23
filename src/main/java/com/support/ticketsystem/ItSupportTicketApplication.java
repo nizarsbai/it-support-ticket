bdb74af36fe36cd15ad8683d9d081b52abc35217
Nizar Sbai <nizar1.sbai@gmail.com>
2025-02-23 22:14:04 +0100

    Made Spring Boot Application to launch the mainTicket.java swing interface

--- src/main/java/com/support/ticketsystem/ItSupportTicketApplication.java
+++ src/main/java/com/support/ticketsystem/ItSupportTicketApplication.java
@@ -1,12 +1,19 @@
 package com.support.ticketsystem;
 
+import com.support.ticketsystem.supportITSW.mainTicket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class ItSupportTicketApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("java.awt.headless", "false"); // Désactiver le mode headless
+		//System.out.println("Is headless: " + java.awt.GraphicsEnvironment.isHeadless());
 		SpringApplication.run(ItSupportTicketApplication.class, args);
+		// Lancer l'interface Swing sur le thread de l'interface graphique
+		SwingUtilities.invokeLater(() -> new mainTicket().setVisible(true));
 	}
 }
