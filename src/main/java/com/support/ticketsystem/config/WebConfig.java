83f95982fb94f68fa6e6eba5889a96cb762a3b17
Nizar Sbai <nizar1.sbai@gmail.com>
2025-02-24 01:47:07 +0100

    Config Mapping CORS

---
+++ src/main/java/com/support/ticketsystem/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.support.ticketsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/tickets/**").allowedOrigins("http://localhost:8080"); // Update with your actual client URL if different
+    }
+}
